6d68f02f3b1ca222241a487fa44c9c36f7a216b2
Connor Doyle <connorjamesdoyle95@gmail.com>
2024-12-21 18:58:03 +0000

    Update styling and layout

--- 01_Porosity Permeability Dash/app.py
+++ 01_Porosity Permeability Dash/app.py
@@ -6,18 +6,54 @@ import dash_bootstrap_components as dbc
 # Define the theme to use - we are using the 'Sandstone' theme from Bootswatch: https://bootswatch.com/sandstone/
 external_stylesheets = [dbc.themes.BOOTSTRAP]
 
+### Data Loading & Cleaning
 # Load the dataframe
-df = pd.read_excel(r'Realpore Data_Trimmed.xlsx')
+df = pd.read_excel(r"C:\Users\conno\Python\PythonforGeoscience\GitHub\Geoscience-in-Dash\01_Porosity Permeability Dash\assets\Realpore Data_Trimmed.xlsx")
 
+# Drop null rows in the porosity (CPH) column
+df.dropna(subset='CPH', inplace=True)
+
+### App Code
 # Define the app and use the previously defined external stylesheet
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 # Making our layout
-app.layout =dbc.Container([
-    html.H1(children='Porosity-Permeability Dash', style={'textAlign':'center'}),
-    dcc.Dropdown(df.Well.unique(), id='well-selection'),
-    dcc.Graph(id='graph-content')
-])
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col(
+            [
+                html.H2(
+                    'Well Selection', 
+                    className='text-light'
+                ),
+
+                dcc.Dropdown(
+                    df.Well.unique(), 
+                    id='well-selection'
+                ),
+            ],  
+        width=2,  # Left column width for dropdown
+        style={'backgroundColor':'#343a40',
+               'padding':'20px',
+               'height':'100vh',
+               'color':'black'
+            }
+    ),
+    # Graph
+        dbc.Col(
+            dcc.Graph(id='graph-content',
+            style={'height':'100%'}
+        ),
+            width=10,
+            style={
+                'height':'100vh',
+                'padding':'0'
+            }
+        )
+                # Center and right column width for the graph
+    ])
+], fluid=True)  # Use `fluid=True` for responsive behavior
+
 
 # Callback to update the graph when the well is changed
 @callback(
@@ -28,7 +64,25 @@ app.layout =dbc.Container([
 # Related function to update graph, using porosity as X col, and permeability as y col, with log-y axis
 def update_graph(value):
     dff=df[df.Well==value]
-    return px.scatter(dff, x='CPH', y='KAH')
+    fig = px.scatter(dff, 
+                      x='CPH', 
+                      y='KAH',
+                      labels={'CPH':'Porosity (%)', 'KAH':'Permeability (mD)'},
+                      log_y=True)#
+    
+    fig.update_traces(
+        marker=dict(
+            size=10,
+            color='black',
+            opacity=0.8,
+            symbol='circle'
+        )
+
+    )
+
+    return fig
 
 if __name__ =='__main__':
     app.run(debug=True)
+
+
